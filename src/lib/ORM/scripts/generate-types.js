cd40ca4c43356ce9224c910421b3ca75c7c554aa
tedwin007 <tedwin007@gmail.com>
2023-01-19 11:11:38 +0200

    fallback to generate-types script

--- src/lib/ORM/scripts/generate-types.js
+++ src/lib/ORM/scripts/generate-types.js
@@ -4,11 +4,12 @@ require('dotenv').config();
 const {  HOST, DB_NAME, USER_NAME, PASSWORD, DB_TYPE, OUTPUT_PATH } = process.env;
 const hasEnvVar =Boolean(HOST && DB_NAME && USER_NAME && PASSWORD && DB_TYPE&& OUTPUT_PATH);
 
+const cmd = 'npx typeorm-model-generator';
+const envCMD = `${cmd} -h ${HOST} -d ${DB_NAME} -u ${USER_NAME} -x ${PASSWORD} -e ${DB_TYPE} -o ${OUTPUT_PATH}`;
+
 if(!hasEnvVar){
-    shell.exec('npx typeorm-model-generator').code !== 0
-}else if ( shell.exec(
-    `npx typeorm-model-generator -h ${HOST} -d ${DB_NAME} -u ${USER_NAME} -x ${PASSWORD} -e ${DB_TYPE} -o ${OUTPUT_PATH}`
-    ).code !== 0) {
+    shell.exec(cmd).code !== 0
+}else if ( shell.exec(envCMD).code !== 0) {
     shell.echo('Error: typeorm model generator failed');
     shell.exit(1);
 }
